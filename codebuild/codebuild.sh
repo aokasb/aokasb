7393a2e44e44ac0c270678c8e4c149c85188726d
Abhijit <abhijit.oka@icf.com>
2023-08-24 14:07:56 -0400

    added source version

--- codebuild/codebuild.sh
+++ codebuild/codebuild.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 CONFIGJSON=${1:-Eligibility.json}
-OVERRIDEDICT=${2:-'{}'}
+SOURCEVERSION=${2:-develop}
+OVERRIDEDICT=${3:-'{}'}
 python jsonOverride.py $CONFIGJSON $OVERRIDEDICT > tmp.json
 
-echo "aws codebuild start-build \
+command="aws codebuild start-build \
 --project-name qppsf-impl-codebuild-manualruns \
 --profile 075503252202_ct-ado-qppscoring-application-admin \
 --environment-variables-override file://./tmp.json \
---source-version feature/udsredshift/QPPSE-895_publish_codebuild"
+--source-version $SOURCEVERSION"
+
+echo $command
+$command 1> codebuild.out 2> codebuild.log
+echo "command executed , codebuild out and log are created" 
