1bcf88188483555c1aeadb32918c930c4ebbb525
Abhijit <abhijit.oka@icf.com>
2023-08-30 17:01:37 -0400

    correcting stdout vs stderr

--- codebuild/jsonOverride.py
+++ codebuild/jsonOverride.py
@@ -3,9 +3,14 @@ import sys
 with open(sys.argv[1],"rb") as f:
     data = json.load(f)
 override=json.loads(sys.argv[2])
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 for i in range(len(data)):
     for j in override:
         if data[i].get("name")==j:
+            r=override[j]
+            o=data[i]["value"]
+            eprint("Found",o," and value ",j," being replaced to ",r)
             data[i]["value"]=override[j]
             break
 print(json.dumps(data,indent=2, sort_keys=False, ensure_ascii=False))
