6ba51e93dab8e032fd96d3e7629ed3241de82739
Abhijit <abhijit.oka@icf.com>
2023-08-31 12:20:53 -0400

    updating codebuild and aws key commands

--- bin/update-aws-key.sh
+++ bin/update-aws-key.sh
@@ -1,23 +1,21 @@
 # Get key from https://cloudtamer.cms.gov/portal/app-api-key
 #!/bin/bash
-ACCNAME=${1:-scimpl}
-case $ACCNAME in 
-	scimpl)
-		ACC="SOMEACC1"
-		ROLE="SOMEROLE1"
-		;;
-	scprod)
-		ACC="SOMEACC1"
-		ROLE="SOMEROLE1"
-		;;
-	*)
-		echo "bad account id"
-		exit 1
-esac
+# NEEDS ~/ctkey to have ctkey-osx executable
+# NEEDS ~/.cloudtamer/config to have account and role associated
+# Populates ~/.cloudtamer/apikey if not present
+# USE source to execute this file so that your environment will get the right role etc
+ACC=""
+ACCNAME=${1:-acc_abbr}
+parser="import configparser,sys;config = configparser.ConfigParser();config.read(sys.argv[1]);c=sys.argv[2];print(config[c]['account'],config[c]['role'])"
+read -r ACC ROLE <<< $(python -c "$parser" ~/.cloudtamer/config $ACCNAME)
+if [ "$ACC" = "" ];then
+  echo "Bad account name check or create the ~/.cloudtamer/config for entries of account and role"
+  exit 1
+fi
 pushd ~/ctkey
 apikey=$(cat ~/.cloudtamer/apikey) 
 ./ctkey-osx savecreds --url=https://cloudtamer.cms.gov --app-api-key=$apikey --account=${ACC} --iam-role=$ROLE
-if [ $? -eq 0 ]; then 
+if [ $? -eq 0 ]; then
 	echo "existing key works"
 else 
 	echo "New Cloudtamer API Key required"
--- codebuild/codebuild.sh
+++ codebuild/codebuild.sh
@@ -1,15 +1,85 @@
 #!/bin/bash
-CONFIGJSON=${1:-Eligibility.json}
-SOURCEVERSION=${2:-develop}
-OVERRIDEDICT=${3:-'{}'}
-python jsonOverride.py $CONFIGJSON $OVERRIDEDICT > tmp.json
+# Objectives: Duplicate the AWS Codebuild / start codebuild from template with change in Environment variable
+
+# Default values
+CONFIGJSON=""
+SOURCEVERSION=develop
+OVERRIDEDICT='{}'
+REFERENCE="NONE"
+PROJECT="project"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -c|--config)
+      CONFIGJSON="$2"
+      shift
+      shift
+      ;;
+    -s|--source)
+      SOURCEVERSION="$2"
+      shift
+      shift
+      ;;
+    -o|--override)
+      OVERRIDEDICT="$2"
+      shift
+      shift
+      ;;
+    -r|--reference)
+      REFERENCE="$2"
+      shift
+      shift
+      ;;
+    -p|--project)
+      PROJECT="$2"
+      shift
+      shift
+      ;;
+    *)
+      echo "Unknown option $1"
+      echo "Usage: bash codebuild.sh
+        -c <environment-variables-json> OR -r <reference-project-run-hash>
+        -o '{\"<override_key\":\"override_value\"}'
+        -p <codebuild-project> Default: manualruns
+        -s <source-version> Default: develop
+        Dependency: jsonOverride.py
+        Dependency: Needs $AWS_PROFILE to be set
+        - Executes standard build when no configuration is specified
+        - only existing parameters can be overriden new parameters cannot be added
+        "
+      exit 1
+      ;;
+
+  esac
+done
+
+if [ "$REFERENCE" != "NONE" ];then
+        echo "Downloading environment variables for reference"
+        aws codebuild batch-get-builds --ids $PROJECT:$REFERENCE --output json | jq '.builds[].environment.environmentVariables' > ref.json
+        CONFIGJSON="ref.json"
+fi
+
+if [ "$CONFIGJSON" != "" ];then
+        echo "applying override - result in tmp.json)"
+        python jsonOverride.py $CONFIGJSON $OVERRIDEDICT 1> tmp.json
+else
+        echo "No config provided only triggering the run"
+        echo '[]' > tmp.json
+fi
 
 command="aws codebuild start-build \
---project-name qppsf-impl-codebuild-manualruns \
---profile 075503252202_ct-ado-qppscoring-application-admin \
+--project-name $PROJECT \
+--profile $AWS_PROFILE \
 --environment-variables-override file://./tmp.json \
---source-version $SOURCEVERSION"
+--source-version $SOURCEVERSION \
+--region us-east-1"
 
 echo $command
 $command 1> codebuild.out 2> codebuild.log
-echo "command executed , codebuild out and log are created" 
+RESULT=$?
+if [ $RESULT -eq 0 ];then
+        echo "command SUCCEEDED, codebuild out and log are created"
+else
+        echo "command FAILED see output and logs"
+
+fi
